d59ec283afad7266d6c9478fbfc4ffa2eb21c71e
zainsarfraz <44726141+zainsarfraz@users.noreply.github.com>
2021-10-21 04:06:53 +0500

    Google Auth Setup

--- gamebyte/urls.py
+++ gamebyte/urls.py
@@ -18,10 +18,11 @@ from django.urls import path
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', googleAuthTest,name='googleAuthTest'),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
 ]
