2cf13dd677e027031a3957fa8e1a25ddb76815bd
zainsarfraz <44726141+zainsarfraz@users.noreply.github.com>
2021-10-21 23:51:28 +0500

    added app-icon.png and made base.html

--- gamebyte/settings.py
+++ gamebyte/settings.py
@@ -128,9 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # oauth
 
