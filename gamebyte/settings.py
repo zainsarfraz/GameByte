1840522bab2fbe54f24034fcd716cbe8106f656c
zainsarfraz <44726141+zainsarfraz@users.noreply.github.com>
2021-10-27 12:31:54 +0500

    Converted Database from sqlite to Postgresql

--- gamebyte/settings.py
+++ gamebyte/settings.py
@@ -84,9 +84,24 @@ WSGI_APPLICATION = 'gamebyte.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': "d5vn424ppni32t",
+
+        'USER': "kqxiyzyokpfkit",
+
+        'PASSWORD': "ec507a3b728b37ff34c687ae5746268fc390ed36ed523208cfcf98cf20d6a73a",
+
+        'HOST': "ec2-52-70-107-254.compute-1.amazonaws.com",
+
+        'PORT': "5432",
+
     }
 }
 
--- gamebyte/views.py
+++ gamebyte/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 
 def googleAuthTest(request):
+    print(request.user)
     return render(request,'index.html')
