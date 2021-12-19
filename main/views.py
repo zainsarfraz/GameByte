5d56a264cd2d5df5bf9b07c5c9b3d50607605537
zainsarfraz <zain.noob99@gmail.com>
2021-12-19 19:09:06 +0500

    test

--- main/views.py
+++ main/views.py
@@ -189,9 +189,10 @@ def runtestcase(request):
                 import stat
                 st = os.stat(filename)
                 os.chmod(filename, st.st_mode | stat.S_IEXEC)
-                proc = subprocess.Popen(
-                    [filename],stdout=subprocess.PIPE,
-                    stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+                # proc = subprocess.Popen(
+                #     [filename],stdout=subprocess.PIPE,
+                #     stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+                subprocess.check_call([sys.executable or 'python',filename],stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
             proc.stdin.write(args)
             proc.stdin.close()
             output,error = proc.communicate()
