4495efe055ab87b9c155e4f485aef893bc255f5d
zainsarfraz <zain.noob99@gmail.com>
2021-12-19 18:04:52 +0500

    test

--- main/views.py
+++ main/views.py
@@ -180,7 +180,7 @@ def runtestcase(request):
 
             # deployed code
             proc = subprocess.Popen(
-                ['chmod','755',filename],stdout=subprocess.PIPE,
+                [filename],stdout=subprocess.PIPE,
                 stdin=subprocess.PIPE,stderr=subprocess.PIPE)
             proc.stdin.write(args)
             proc.stdin.close()
