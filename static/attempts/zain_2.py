3d8980e763c4b7e85a843c5cbbc406e22799e509
zainsarfraz <zain.noob99@gmail.com>
2021-12-21 00:57:55 +0500

    fixed problem description

--- static/attempts/zain_2.py
+++ static/attempts/zain_2.py
@@ -1,3 +1,7 @@
-x = []
-for i in range(10):
-    x.append(i)
+#!/usr/bin/env python
+# Do not change this line.
+snakeX, snakeY, fruitX, fruitY = map(str,input().split(','))
+
+# You have access to the following variables:
+# snakeX, snakeY, fruitX, fruitY
+# Write your code here
