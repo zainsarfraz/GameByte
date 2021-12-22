721cc31f3c6ec7fbcfad72b4aaeb2b8802b2e69d
Maaz Javed <maaz.ravian@gmail.com>
2021-12-22 15:47:53 +0500

    snake game working

---
+++ static/attempts/bitf18a013-maaz_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Do not change this line.
+snakeX, snakeY, fruitX, fruitY = map(str,input().split(','))
+
+# You have access to the following variables:
+# snakeX, snakeY, fruitX, fruitY
+# Write your code here
+print(0)
+print(0)
+print(0)
+print(0)
+print(0)
+print(1)
+print(0)
+print(0)
+print(0)
