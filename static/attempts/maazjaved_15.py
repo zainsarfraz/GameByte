05ed5a3620e043832606eb7f5f087eca390d28e1
Maaz Javed <maaz.ravian@gmail.com>
2022-06-15 14:06:27 +0500

    dashboard.html

---
+++ static/attempts/maazjaved_15.py
@@ -0,0 +1,7 @@
+# Do not change this line.
+snakeX, snakeY, fruitX, fruitY = map(int,input().split(','))
+
+# You have access to the following variables:
+# snakeX, snakeY, fruitX, fruitY
+# Write your code here
+print("Hello World")
